e79cf5182e4fe45e9a50d4d32626af4c68b57e63
Ramkumar <ramkumar@railsfactory.org>
2012-04-17 14:27:53 +0530

    order model added

--- app/models/order_decorator.rb
+++ app/models/order_decorator.rb
@@ -12,5 +12,37 @@ Order.class_eval do
     end
 end
   end
+state_machines.clear
+        state_machines[:state] = StateMachine::Machine.new(Order, :initial => 'cart', :use_transactions => false) do 
+          event :next do 
+            transition :from => 'cart', :to => 'one_page'
+            transition :from => 'one_page', :to => 'complete' 
+          end 
+          event :cancel do 
+            transition :to => 'canceled', :if => :allow_cancel? 
+          end 
+          event :return do 
+            transition :to => 'returned', :from => 'awaiting_return' 
+          end 
+          event :resume do 
+            transition :to => 'resumed', :from => 'canceled', :if => :allow_resume? 
+          end 
+          event :authorize_return do 
+            transition :to => 'awaiting_return' 
+          end 
+          before_transition :to => 'complete' do |order| 
+            begin 
+              order.process_payments! 
+            rescue Spree::GatewayError 
+              if Spree::Config[:allow_checkout_on_gateway_error] 
+                true 
+              else 
+                false 
+              end 
+            end 
+          end 
+          after_transition :to => 'complete', :do => :finalize! 
+          after_transition :to => 'canceled', :do => :after_cancel
+        end 
 
 	end
