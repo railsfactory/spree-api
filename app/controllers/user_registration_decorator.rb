3dfeefc15aa9f4ee7aa0da92c5e8f2ac02e7a41d
Ramkumar <ramkumar@railsfactory.org>
2012-04-30 20:18:01 +0530

    user sign_up

--- app/controllers/user_registration_decorator.rb
+++ app/controllers/user_registration_decorator.rb
@@ -7,26 +7,32 @@
   before_filter :check_permissions, :only => [:edit, :update]
   skip_before_filter :require_no_authentication
     $e6={"status_code"=>"500","status_message"=>"sorry email already taken"}
+    $e14={"status_code"=>"500","status_message"=>"password miss match"}
  #~ prepend_before_filter :load_object, :only => [:show, :edit, :update]
   #~ prepend_before_filter :authorize_actions, :only => :new
 def create
   if !params[:format].nil? && params[:format] == "json"
-   @user = build_resource(params[:user])
-    logger.debug(@user)
-    if resource.save
-      #~ @role=Role.find_by_id(2)
-       #~ @user.roles<< @role
+    if params[:user][:email]!=nil
+    if params[:user][:password]==params[:user][:password_confirmation]
+    @user=User.new(params[:user])
+    if @user.save
     render :json =>@user
     else
       error=error_response_method($e6)
       render:json=>error
     end
     else
+      error=error_response_method($e14)
+      render:json=>error
+    end
+    else
+       error=error_response_method($e1)
+      render:json=>error
+      end
+    else
        @user = build_resource(params[:user])
     logger.debug(@user)
     if resource.save
-      #~ @role=Role.find_by_id(2)
-       #~ @user.roles<< @role
       set_flash_message(:notice, :signed_up)
       sign_in_and_redirect(:user, @user)
     else
@@ -34,29 +40,7 @@ def create
       render_with_scope(:new)
     end
   end
-	  #~ if !params[:format].nil? && params[:format] == "json"
-    #~ @user = User.new(params[:user])
-    #~ if @user.save
-  #~ render :json =>@user
-  #~ else 
-    #~ error=error_response_method($e6)
-        #~ render:json=>error
- #~ end
-#~ else
-	#~ @user = User.new(params[:user])
-    #~ if @user.save
-
-      #~ if current_order
-        #~ current_order.associate_user!(@user)
-        #~ session[:guest_token] = nil
-      #~ end
-
-      #~ redirect_back_or_default(root_url)
-    #~ else
-      #~ render 'new'
-    #~ end
-
-	#~ end
+	  
 end
 def check_permissions
     authorize!(:create, resource)
