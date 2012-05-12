4b37be08b01370f00ecd54c5c47e39326a6ac3b8
Ramkumar <ramkumar@railsfactory.org>
2012-05-12 20:08:01 +0530

    model

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,20 +1,22 @@
 Spree::User.class_eval do
   def generate_api_key!
     self.api_key = SecureRandom.hex(24)
+      self.reset_authentication_token!
     save!
   end
 
   def clear_api_key!
     self.api_key = nil
+       self.update_attribute(:authentication_token, "")
     save!
   end
-  def clear_api_key!
-    self.update_attribute(:authentication_token, "")
-  end
+  #~ def clear_api_key!
+    #~ self.update_attribute(:authentication_token, "")
+  #~ end
 
-  def generate_api_key!
-    self.reset_authentication_token!
-  end
+  #~ def generate_api_key!
+    #~ self.reset_authentication_token!
+  #~ end
    private
 
   def secure_digest(*args)
