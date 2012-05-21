b3fbbffca18e4d6bcf00883a78cfabcbd91812aa
Ramkumar <ramkumar@railsfactory.org>
2012-05-21 18:15:00 +0530

    search taxon

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,4 @@
-class Spree::SearchController< Spree::BaseController
+class SearchController< Spree::BaseController
 	rescue_from ActiveRecord::RecordNotFound, :with => :render_404
     helper 'spree/products'
 def search
@@ -12,5 +12,17 @@ def search
 			else
 				respond_with(@taxon)
 				end
-    end
+			end
+					def taxon
+						if !params[:id].present?
+		 @taxon=Spree::Taxon.where("parent_id IS NULL")
+		 render :json => @taxon.to_json, :status => 201
+		 else
+			 #@taxon=Spree::Taxon.find(:all,:conditions=>["parent_id  like ?","%#{params[:id]}%"])
+			 @taxon=Spree::Taxon.find_all_by_parent_id(params[:id])
+			 render :json => @taxon.to_json, :status => 201
+			 end
+	 end
+	 #~ def child_taxon
+		 #~ @taxon=Spree::Taxon.find_by_id
     end
