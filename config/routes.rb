6600d2c8e5c54b036d61ab9db1d6f075033c4873
Ramkumar <ramkumar@railsfactory.org>
2012-05-22 14:50:29 +0530

    rou

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Spree::Core::Engine.routes.draw do
   resources :countries do
       resources :states
     end
-    match '/checkout' => 'checkout#edit', :state => 'one_page', :as => :checkout
+    #match '/checkout' => 'checkout#edit', :state => 'one_page', :as => :checkout
       #resources :users , :except => [:new,:edit]
        devise_scope :user do
     match '/users' => 'user_registrations#create',:via=>:post
