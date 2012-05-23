8adf523f4bd859500bfa054e62181072606c4c38
Ramkumar <ramkumar@railsfactory.org>
2012-05-23 18:33:23 +0530

    user

--- app/controllers/user_registration_decorator.rb
+++ app/controllers/user_registration_decorator.rb
@@ -45,13 +45,15 @@ else
       
     else
       @user = build_resource(params[:user])
-      logger.debug(@user)
-      if resource.save
-        set_flash_message(:notice, :signed_up)
-        sign_in_and_redirect(:user, @user)
-      else
-        clean_up_passwords(resource)
-        render_with_scope(:new)
+    if resource.save
+      set_flash_message(:notice, :signed_up)
+      sign_in(:user, @user)
+      fire_event('spree.user.signup', :user => @user, :order => current_order(true))
+      sign_in_and_redirect(:user, @user)
+    else
+      clean_up_passwords(resource)
+      render :new
+    end
       end
     end
 	  
