126e8474520df763492130721ebc85cf691b6f95
Ramkumar <ramkumar@railsfactory.org>
2012-05-25 22:03:23 +0530

    spec 2

--- spec/controller/spree/user_sessions_controller_decorator_spec.rb
+++ spec/controller/spree/user_sessions_controller_decorator_spec.rb
@@ -1,24 +1,33 @@
 require 'spec_helper' 
 
 describe   Spree::UserSessionsController,:type=>"controller" do
-  #include Devise::TestHelpers
-  describe " UserSessionsController" do
-    let(:user) { build(:user) }
-    def mock_user(stubs={})
-      @mock_user ||= mock_model(Spree::User, stubs).as_null_object
+	#include Devise::TestHelpers
+	describe " UserSessionsController" do
+		 let(:user) { build(:user) }
+			def mock_user(stubs={})
+        @mock_user ||= mock_model(Spree::User, stubs).as_null_object
     end
     before(:each) do
-      
-      request.env['warden'] = mock(Warden, :authenticate => mock_user,:authenticate! => mock_user)
-      
-      @current_user = 1
-    end
-    it "should be successful" do
-      post :create,{:user=>{:email=>"spree@example.com", :password=>"1234564"}},:format=>:json
-      response.code.should == "200"
-    end
-    it "it should destroy the user" do
-      get :destroy, {:authentication_token=>""}, :format => :json
-    end
-  end
-end
+        request.env['warden'] = mock(Warden, :authenticate => mock_user)
+				request.env["devise.mapping"] = Devise.mappings[:user]
+				@current_user = 1
+				controller.stub!(:authenticate_user!).and_return(true)
+			end
+		#~ it "should be successful" do
+			#~ @user=Spree::User.create(:email => "email@person.com", :password => "secret", :password_confirmation => "secret")
+			#~ @auth=@user.authentication_token="16qL-TsdhrJmHGWCC8bS"
+			#~ @user.save
+			#~ post :create , :user=>{:email => "email123@person.com", :password => "secret", :password_confirmation => "secret"} , :format => :json
+			 #~ response.code.should == "200"
+		#~ end
+				 
+	it "should delete the user" do
+		@user=Spree::User.create(:email => "email@person.com", :password => "secret", :password_confirmation => "secret")
+					@auth=@user.authentication_token="16qL-TsdhrJmHGWCC8bS"
+					@user.save
+					get :destroy, {:id => @user.to_param,:format => :json}
+				response.code.should == "200"
+			end
+			
+		end
+		end
